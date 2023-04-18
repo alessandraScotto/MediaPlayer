a3c4f3adb5261678d14bf18813319fae62c22cf9
IdaAlessandra ScottodiLuzio <idaalessandra.scottodiluzio@gmail.com>
2023-04-18 20:51:26 +0200

    Funzioni play, stop, back, next

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ let wrapper = document.querySelector('#wrapper');
 let i = 0;
 //Sezione audio e cover
 function createCover() {
-
+    wrapper.innerHTML = '';
     let div = document.createElement('div');
     div.classList.add('col-12', 'col-md-4');
     div.innerHTML = ` 
@@ -24,43 +24,92 @@ function createCover() {
           <source src="${tracks[i].url}" type="audio/mpeg">
         </audio>`
     wrapper.appendChild(div);
-}
 
+    audio = document.querySelector('audio');
+}
+//Sezione info track
 function createInfoTrack() {
 
     let div = document.createElement('div');
     div.classList.add('col-12', 'col-md-8');
     div.innerHTML = `<div
   class="d-flex justify-content-center align-items-center flex-column mb-5 text-white fw-lighter">
-  <h3 class="mb-4 fontMain">Titolo canzone</h3>
-  <h3 class="fontMain">Autore</h3>
-</div>
-<div class="progress">
+  <h3 class="mb-4 fontMain">${tracks[i].title}</h3>
+  <h3 class="fontMain">${tracks[i].artist}</h3>
+   </div>
+   <div class="progress">
   <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25"
       aria-valuemin="0" aria-valuemax="100"></div>
-</div>
-<!-- Tempo Inizio e Fine -->
-<div class="d-flex justify-content-between">
+  </div>
+  <!-- Tempo Inizio e Fine -->
+  <div class="d-flex justify-content-between">
   <p id="start" class="text-white fw-semibold">0:00</p>
   <p id="end" class="text-white fw-semibold">00.00</p>
-</div>
+  </div>
 
-<!-- Sezione Pulsanti AUDIO -->
-<div class="d-flex justify-content-between">
+  <!-- Sezione Pulsanti AUDIO -->
+  <div class="d-flex justify-content-between">
   <button id="btnBackward" class="btn btn-outline-light fs-3"><i
           class="fa-solid fa-backward"></i></button>
   <button id="btnPlay" class="btn btn-outline-light fs-3"><i
           class="fa-solid fa-play"></i></button>
   <button id="btnForward" class="btn btn-outline-light fs-3"><i
           class="fa-solid fa-forward"></i></button>
-</div>
-`
+  </div>
+  `
     wrapper.appendChild(div);
+
+
+    //Button Play, Next, Prev, Pause.
+    //Catturare pulsanti audio
+    let btnBackward = document.querySelector('#btnBackward');
+    let btnPlay = document.querySelector('#btnPlay');
+    let btnForward = document.querySelector('#btnForward');
+
+    //Funzione play and pause
+    btnPlay.addEventListener('click', () => {
+
+        if (audio.paused == true) {
+            audio.play();
+            btnPlay.innerHTML = '<i class="fa-solid fa-pause"></i>';
+        } else {
+            audio.pause();
+            btnPlay.innerHTML = '<i class="fa-solid fa-play"></i>';
+        }
+    })
+
+    //Funzione Next song
+    btnForward.addEventListener('click', () => {
+       if (i < tracks.length - 1) {
+        i++
+        createCover();
+        createInfoTrack();        
+       } else {
+        i = 0;
+       }
+    });
+
+    //Funzione Backward song
+    btnBackward.addEventListener('click', () => {
+      if (i > 0) {
+        i--;
+       createCover();
+       createInfoTrack();  
+      } else {
+        i = tracks.length - 1;
+      }
+    })
+
+
+
 
 }
 
 
-createCover();  //Invochiamo la funzione per la creazione del div Cover e Audio
 
+
+
+
+createCover();  //Invochiamo la funzione per la creazione del div Cover e Audio
 createInfoTrack(); //Invochiamo la funzione per la creazione delle info sulla traccia audio e progress-bar
 
